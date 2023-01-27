52ad4b3f55334bb43847906acbe7cee26b499281
mts2200 <mts2200@rambler.ru>
2023-01-28 01:26:36 +0300

    Fix LateMixin loading issue.
    Now, classes implements ILateMixinLoader require to be marked with the LateMixin annotation

---
+++ src/main/java/io/github/tox1cozz/mixinbooterlegacy/LateMixin.java
@@ -0,0 +1,15 @@
+package io.github.tox1cozz.mixinbooterlegacy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+		ElementType.ANNOTATION_TYPE,
+		ElementType.CONSTRUCTOR,
+		ElementType.FIELD,
+		ElementType.METHOD,
+		ElementType.TYPE})
+public @interface LateMixin {}
