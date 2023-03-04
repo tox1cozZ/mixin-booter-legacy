044ee666320da4ed0fcdcf71cd9d92b9e300bad1
tox1cozZ <Agravaine25@yandex.ru>
2023-03-04 14:22:16 +0200

    1.2.1

--- src/main/java/io/github/tox1cozz/mixinbooterlegacy/MixinBooterLegacyPlugin.java
+++ src/main/java/io/github/tox1cozz/mixinbooterlegacy/MixinBooterLegacyPlugin.java
@@ -88,7 +88,7 @@ public final class MixinBooterLegacyPlugin implements IFMLLoadingPlugin {
             ModMetadata meta = getMetadata();
             meta.modId = "mixinbooterlegacy";
             meta.name = "MixinBooterLegacy";
-            meta.version = "1.2.0";
+            meta.version = "1.2.1";
             meta.description = "A Mixin library and loader.";
             meta.logoFile = "/icon.png";
             meta.authorList.addAll(Lists.newArrayList("Rongmario", "tox1cozZ"));
